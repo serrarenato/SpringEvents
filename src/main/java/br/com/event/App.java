6dc24c02891c62264703ca99773ccc3ba7e2b255
Renato <serra.renato@gmail.com>
2018-12-18 13:54:16 -0200

    ok

---
+++ src/main/java/br/com/event/App.java
@@ -0,0 +1,28 @@
+package br.com.event;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableAsync
+public class App {
+
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(App.class)
+				.headless(false).run(args);
+
+		CustomEventPublisher cvp = (CustomEventPublisher) context.getBean("customEventPublisher");
+
+		cvp.publish();
+		cvp.publish();
+	}
+}
---
+++ src/main/java/br/com/event/CustomEventPublisher.java
@@ -0,0 +1,19 @@
+package br.com.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+@Component("customEventPublisher")
+public class CustomEventPublisher implements ApplicationEventPublisherAware {
+	private ApplicationEventPublisher publisher;
+
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public void publish() {
+		CustomEvent ce = new CustomEvent(this);
+		publisher.publishEvent(ce);
+	}
+}
